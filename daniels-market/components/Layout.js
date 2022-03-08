1de7dfa63785bf05761ab3153c468f63e1020d93
Daniel Ferreira da Costa Pinto <91293389+danielfcp190@users.noreply.github.com>
2022-03-08 16:57:11 -0300

    developed UI integrated with mongoDB

---
+++ daniels-market/components/Layout.js
@@ -0,0 +1,110 @@
+import React, { useContext } from "react";
+import Head from "next/head";
+import NextLink from "next/link";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Container,
+  Link,
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
+  Switch,
+  Badge,
+} from "@material-ui/core";
+import useStyles from "../utils/styles";
+import { Store } from "../utils/Store";
+import jsCookie from "js-cookie";
+
+export default function Layout({ title, description, children }) {
+  const { state, dispatch } = useContext(Store);
+  const { darkMode, cart } = state;
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      body1: {
+        fontWeight: "normal",
+      },
+    },
+    palette: {
+      type: darkMode ? "dark" : "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
+  const classes = useStyles();
+  const darkModeChangeHandler = () => {
+    dispatch({ type: darkMode ? "DARK_MODE_OFF" : "DARK_MODE_ON" });
+    const newDarkMode = !darkMode;
+    jsCookie.set("darkMode", newDarkMode ? "ON" : "OFF");
+  };
+  return (
+    <div>
+      <Head>
+        <title>
+          {title ? `${title} - Daniel's Market` : "Daniel's Market"}
+        </title>
+        {description && <meta name="description" content={description} />}
+      </Head>
+
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/" passHref>
+              <Link>
+                <Typography className={classes.brand}>
+                  Daniel&apos;s Market
+                </Typography>
+              </Link>
+            </NextLink>
+            <div className={classes.grow}></div>
+            <div>
+              <Switch
+                checked={darkMode}
+                onChange={darkModeChangeHandler}
+              ></Switch>
+              <NextLink href="/cart" passHref>
+                <Link>
+                  {cart.cartItems.length > 0 ? (
+                    <Badge
+                      color="secondary"
+                      badgeContent={cart.cartItems.length}
+                    >
+                      Cart
+                    </Badge>
+                  ) : (
+                    "Cart"
+                  )}
+                </Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+
+        <Container className={classes.main}>{children}</Container>
+
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Daniel&apos;s Market.</Typography>
+        </footer>
+      </ThemeProvider>
+    </div>
+  );
+}
